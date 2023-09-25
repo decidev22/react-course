ce1280ad7694af1ad9e8d5b141dffc688c11df0b
decidev22 <hamento22@gmail.com>
2023-09-25 22:21:55 +1300

    initialise part2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import Backdrop from "./components/Backdrop";
-import Todo from "./components/Todo";
-import Modal from "./components/Modal";
-
 function App() {
-  return (
-    <div>
-      <h1>To Dos</h1>
-      <Todo title="title" />
-      <Todo title="title2" />
-      <Todo title="title3" />
-      <Modal />
-      <Backdrop />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
+
+// import Backdrop from "./components/Backdrop";
+// import Todo from "./components/Todo";
+// import Modal from "./components/Modal";
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>To Dos</h1>
+//       <Todo title="title" />
+//       <Todo title="title2" />
+//       <Todo title="title3" />
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+
+// import "./index.css";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<App />);
